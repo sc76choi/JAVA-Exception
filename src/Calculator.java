3556f4b2a4790ae58cdc24808c0cb666844a3d60
dasom037 <dasom037@gmail.com>
2022-02-02 11:04:19 +0900

    java exception

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,27 @@
-
 public class Calculator {
+	
+	public Calculator() {
+				
+	}
 
+	public static int add(int x, int y) throws 천을_넘는_예외, 음수가_되는_예외 {
+		int result = x + y;
+		if(result > 1000) {
+			throw new 천을_넘는_예외();
+		}
+		if(result < 0) {
+			throw new 음수가_되는_예외();
+		}
+		return result;
+	}
+
+	public static int sub(int x, int y) {
+		return x-y;
+	}
+	public static int multi(int x, int y) {
+		return x*y;
+	}
+	public static int div(int x, int y) {
+		return x/y;
+	}
 }
